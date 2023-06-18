a1fa16d6c62b1444ef104e64f9c599151c117b41
Nasuha Asri <nasuhasri00@gmail.com>
2023-06-19 00:19:16 +0800

    add graph

--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from .models import Switches
+from datetime import datetime, date, timedelta
 
 def index(request):
     switches = Switches.objects.all()
@@ -49,8 +50,67 @@ def display_graph(request):
     switch2 = Switches.objects.filter(sw="S2").values()
     switch3 = Switches.objects.filter(sw="S3").values()
     
-    return render(request, "graph.html", {
-        "switch1": switch1,
-        "switch2": switch2,
-        "switch3": switch3,
-    })
+    status1 = []
+    switchTime1 = []
+    for single_switch in switch1:
+        status1.append(single_switch['status'])
+        
+        timestamp = int(single_switch['ts'])
+        
+        # Convert the Unix timestamp to a datetime object
+        dt = datetime.fromtimestamp(timestamp)
+
+        # Extract the time component from the datetime object
+        time = dt.time()
+        startTime = time.hour
+        endTime = time.hour + 12
+        
+        if startTime <= time.hour < endTime:
+            switchTime1.append(time)
+            
+    status2 = []
+    switchTime2 = []
+    for single_switch in switch2:
+        status2.append(single_switch['status'])
+        
+        timestamp = int(single_switch['ts'])
+        
+        # Convert the Unix timestamp to a datetime object
+        dt = datetime.fromtimestamp(timestamp)
+
+        # Extract the time component from the datetime object
+        time = dt.time()
+        startTime = time.hour
+        endTime = time.hour + 12
+        
+        if startTime <= time.hour < endTime:
+            switchTime2.append(time)
+    
+    status3 = []
+    switchTime3 = []
+    for single_switch in switch3:
+        status3.append(single_switch['status'])
+        
+        timestamp = int(single_switch['ts'])
+        
+        # Convert the Unix timestamp to a datetime object
+        dt = datetime.fromtimestamp(timestamp)
+
+        # Extract the time component from the datetime object
+        time = dt.time()
+        startTime = time.hour
+        endTime = time.hour + 12
+        
+        if startTime <= time.hour < endTime:
+            switchTime3.append(time)
+            
+    context = {
+        "status1": status1,
+        "switchTime1": switchTime1,
+        "status2": status2,
+        "switchTime2": switchTime2,
+        "status3": status3,
+        "switchTime3": switchTime3
+    }
+    
+    return render(request, "graph.html", context)
